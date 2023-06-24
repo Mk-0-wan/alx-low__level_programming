ce6f79d82a19178b053b0b242368f39fee0067bc
mkwan <wan@example.com>
2023-06-24 21:33:01 +0300

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adding two param together
+ *
+ * @x: first param provided
+ * @y: second param position
+ *
+ * Return: adds the two param together
+ */
+int add(int x, int y)
+{
+	return ((x + y));
+}
