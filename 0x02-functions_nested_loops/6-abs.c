89ea0abfa4f624e4f46e79cd400095c776bb0133
mkwan <wan@example.com>
2023-06-24 13:30:18 +0300

    fixed

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * _absolute - Entry point
+ * _abs - Entry point
  *
  * @num: holding the int value to be converted into an absolute value
  *
- * Return - void just returns the absolute value of the param passed
+ * Return: void just returns the absolute value of the param passed
  */
 int _abs(int num)
 {
-	return abs(num);
+	return (abs(num));
 }
