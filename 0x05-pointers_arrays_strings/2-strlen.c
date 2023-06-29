1505c82cb821d5238442255ec094713eaf08ae59
mkwan <wan@example.com>
2023-06-29 17:37:19 +0300

    second

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlne - calculate the length of chars
+ * @s: param for the string passed 
+ * Return: value of length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	
+	while (*s++)
+	{
+		length++;
+	}
+	return length;
+}
