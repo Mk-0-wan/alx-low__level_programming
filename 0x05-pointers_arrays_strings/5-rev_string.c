e05620cb8af520dc2cdbaff205ed0253fd05206a
mkwan <wan@example.com>
2023-06-30 11:54:11 +0300

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reversing the string
+ * @s: pointer to the memory adress of the param
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char rev;
+
+	while (s[index++])
+	{
+		len++;
+	}
+
+	for (index = len - 1; index >= len / 2; index++)
+	{
+		rev = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = rev;
+	}
+}
