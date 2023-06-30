2d4d051215db0f1b785346050082be264b60ea6d
mkwan <wan@example.com>
2023-06-30 10:11:30 +0300

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - output the reverse order of a string
+ * @s: placeholder for the string passed as an argument
+ *
+ * Return: void only prints out the result
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	while (s[len] != '\0')
+	{
+		len++
+	}
+
+	for (len -= 1; len >= 0; len--)
+	{
+		_putchar(len);
+	}
+	_putchar('\n');
+}
