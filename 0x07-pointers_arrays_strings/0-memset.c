25d4f6fd0d081f2f6a2754b8ce7e8cc984a2ee80
mkwan <wan@example.com>
2023-07-04 22:25:03 +0300

    working with strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,14 @@
  * @b:chars to be repeated
  * @n:size of the array
  * Return: pointer
- */ 
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
-	for (i = 0;i < n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-	return s;
+	return (s);
 }
