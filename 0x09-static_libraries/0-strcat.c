5280ac97734cab829f273f8efd30a5c671b0ff4c
mkwan <wan@example.com>
2023-07-11 19:26:50 +0300

    he was almost fogotten

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * strcat - Entry point adding two
+ * strings together
+ * @dest:where to add to
+ * @src:second one to add to
+ * Return: the two string attached together
+ */
+char *_strcat(char *dest, char *src)
+{
+}
