87fc56beafe00e9d0c0776eb0645a72752338ecf
mkwan <wan@example.com>
2023-07-11 19:35:30 +0300

    he was almost fogotten

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strcat - Entry point adding two
+ * _strcat - Entry point adding two
  * strings together
  * @dest:where to add to
  * @src:second one to add to
@@ -8,4 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int i;
+	int j;
+
+	i = 0;
+	while (; dest[i] = '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (; src[j])
+	{
+		j++;
+	}
+
+	dest = '\0';
+	return (dest);
 }
