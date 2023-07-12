dd846605bf822a2ecf99289dcd350fd74f9c2814
Mk-0-wan <victormurithi2020@gmail.com>
2023-07-12 16:18:41 -0600

    fixed error 101

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: whole array of arguments passed
  * Return: Always 0 (success)
  */
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
