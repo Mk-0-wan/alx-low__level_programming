3cff061cccbf02c1d7ea2f7a499cce54a16c4e28
mkwan <wan@example.com>
2023-07-13 03:10:33 +0300

    all good

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -45,9 +45,9 @@ int main(int argc, char *argv[])
 	{
 		if (check_num(argv[count]))
 		{
-			str_to_int = atoi(argv[count])
+			str_to_int = atoi(argv[count]);
 			sum += str_to_int;
-		}	
+		}
 		else
 		{
 			printf("Error\n");
