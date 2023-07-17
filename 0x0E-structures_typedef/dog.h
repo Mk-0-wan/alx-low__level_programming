d1e292d780144bd1d3bd970c65c839e673e5dc47
Mk-0-wan <victormurithi2020@gmail.com>
2023-07-18 01:16:01 +0300

    Having a pet dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - data types for class dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ * 
+ * Description: a data type that stores the
+ * information about a dog and the owner
+ */
 struct dog
 {
 	char *name;
