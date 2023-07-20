b6b55bef71ed6ecb14a6ef73129c744e38ad88cc
Mk-0-wan <victormurithi2020@gmail.com>
2023-07-20 16:40:59 -0400

    string delimeter

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints out the string arguments passed
+ * @separator: delimeter of the string
+ * @n: number of arguments
+ * Return: void
+ */
+void print_strings(char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+	/*start from the first argument*/
+	for (i = 1; i <= n; i++)
+	{
+		/*store the strings in a pointer*/
+		str = va_arg(ap, char*);
+		/*check if the pointer is empty*/
+		if (str == NULL)
+			printf("nil");
+		else
+			printf("%s", str);
+		/*add separator*/
+		if (separator != NULL && i != n)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(ap);
+	printf("\n");
+}
