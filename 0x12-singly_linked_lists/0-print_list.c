94f65c38fed505c45a3dddf66079cab210322b72
archMk <vic@archmk.com>
2023-07-27 22:56:01 +0300

    Printing linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the first element in the list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int j = 0, i = 0;
+
+	/* casting away the const in order to traverse through linked list*/
+	list_t* curr = (list_t*)h;
+	i = curr->len;
+
+	while (curr != NULL)
+	{
+		if (curr->str == NULL)
+		{
+			printf("[%d] (nil)\n", i);
+		}
+		printf("[%d] %s\n", i, curr->str);
+		curr = curr->next;
+		j++;
+	}
+	return (j);
+}
