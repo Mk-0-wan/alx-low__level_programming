74d2730ea81e8575e447ad579fa15743713552bd
archMk <vic@archmk.com>
2023-08-02 00:58:05 +0300

    reverse them

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,21 +7,19 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *curr;
 	listint_t *next = NULL;
 
-	if (!*head || !head) /*checking if Node list is empty*/
+	if (!head || !*head) /*checking if Node list is empty*/
 	{
 		return (NULL);
 	}
-	curr = *head;
-	while (curr)
+	while (*head)
 	{
-		next = curr->next; /*make next pointer to point to another node*/
-		curr->next = prev; /*point backwards instead of foward*/
-		prev = curr; /*move the curr to prev node*/
-		curr = next; /*update the current node to next then repeat the loop*/
+		next = (*head)->next; /*make next pointer to point to another node*/
+        (*head)->next = prev; /*point backwards instead of foward*/
+		prev = *head; /*move the curr to prev node*/
+        *head = next; /*update the current node to next then repeat the loop*/
 	}
-	curr = prev;
-	return (curr);
+	*head = prev;
+	return (*head);
 }
