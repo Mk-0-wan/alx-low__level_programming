e730a9fc2f44d506b0a906b99027acc15a92a711
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-08-01 15:25:55 +0300

    Print them all

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,16 +7,19 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *curr = *head;
-
-	if (new_node == NULL)
+	listint_t *new_node;
+	
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+	}
+	else
 	{
 		return (NULL);
 	}
-
-	new_node->next = NULL;
-	new_node->n = n;
 
 	if (curr != NULL)
 	{
