f00139211bdedf1a59ca906a156f2e2bebbc6071
archMk <vic@archmk.com>
2023-08-02 00:10:12 +0300

    add me to my position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,29 +10,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int count = 0;
 	listint_t *curr, *new_node;
-	/* check if the nodelist is empty*/
-	if (!*head)
+
+	if (!*head) /*check if the nodelist is empty*/
 	{
 		return (NULL);
 	}
 
 	curr = *head;
-	/* check if there are no elements add this one as the first one*/
-	if (idx == 0)
+	if (idx == 0) /*check if there are no elements add this one as the first one*/
 	{
 		new_node = malloc(sizeof(listint_t));
 		if (!new_node)
 		{
 			return (NULL);
 		}
-		new_node->n = n; /* set the value */
+		new_node->n = n; /* set the value*/
 		new_node->next = curr;
-		*head = new_node; /* make the new header node */
+		*head = new_node; /* make the new header node*/
 		return (new_node);
 	}
-	while (curr) /* back of the node list and traversing through it */
+	while (curr) /*back of the node list and traversing through it*/
 	{
-		if (count + 1 == idx) /* start counting from the first indexed element */
+		if (count + 1 == idx) /*start counting from the first indexed element*/
 		{
 			new_node = malloc(sizeof(listint_t));
 			if (!new_node)
