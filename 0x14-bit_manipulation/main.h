cae4089b4c4d30c1a8ecc49c6b3ce775614ea6fb
archMk <vic@archmk.com>
2023-08-05 00:12:14 +0300

    Putchar.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 #include <stdlib.h>
 #include <stdio.h>
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,4 +9,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+int _putchar(char c);
 #endif
