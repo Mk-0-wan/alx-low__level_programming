4e652697d763f47027dda861c8ba2832c7891cd4
archMk <victormurithi2020@gmail.com>
2023-08-31 00:19:55 +0300

    square root function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,17 +4,21 @@
  * @value: value given to find the square root
  * @guess: multiplied together and check if it matches the value passed
  * product of two identical number will always give you the square number
- * Will use that to our advantage to guess the x values for value
+ * @precision: used to make the guessing have a limit of how
+ * far they should be from the correct square root value
  * Return: int value found matching the n value when multiplied
  * together.
  */
-int sqrT(int value, int guess)
+int sqrT(int value, int guess, int precision)
 {
-	if ((guess * guess) < value) /* no square root found */
-		return (-1);
-	if ((guess * guess) == value) /* square root found (x)**2 */
+	int x;
+
+	if ((guess * guess) >= value && (guess * guess - value) <= precision)
+	{
 		return (guess);
-	return (sqrT(value, (guess + 1)));
+	}
+	x = sqrT(value, (guess + value / guess) / 2, precision);
+	return (x);
 }
 /**
  * _sqrt_recursion - returns the square root of a given number
@@ -25,7 +29,27 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1); /* can't handle negative numbers */
-
+	if (n == 0 || n == 1)
+		return (1);
 	/* call another recursive function */
-	return (sqrT(n, 0));
+	return (sqrT(n, n / 2, 0));
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+
+	r = _sqrt_recursion(16777216);
+	printf("%d\n", r);
+	r = _sqrt_recursion(1);
+	printf("%d\n", r);
+	r = _sqrt_recursion(4096);
+	printf("%d\n", r);
+	return (0);
 }
