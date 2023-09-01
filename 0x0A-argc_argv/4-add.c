99b31f3c4d37444ac50bc2d27a5baf7c103e842b
archMk <victormurithi2020@gmail.com>
2023-09-02 02:45:22 +0300

    Multiplying arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _atoi - converting string to int
- * @s: pointer to a string
- * Return: converted value or 0 on error
- */
-int _atoi(char *s)
-{
-	int sign = 1, i = 0;
-	unsigned int result = 0;
-
-	/* look for all the negative values inside the string */
-	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
-	{
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-		i++;
-	}
-	/* look for all the non-negative values inside the string */
-	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
-	{
-		result = (result * 10) + (s[i] - '0');
-		i++;
-	}
-	result *= sign;
-	return (result);
-}
-
-/**
  * main - add all the arguments together
  * @argc: arguments count
  * @argv: list of arguments passed
@@ -41,12 +12,12 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!_atoi(argv[i]))
+		if (!atoi(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		result += _atoi(argv[i]);
+		result += atoi(argv[i]);
 	}
 	printf("%d\n", result);
 	return (0);
