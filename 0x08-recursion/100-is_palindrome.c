e6ff7654df0a6145f037b2f4815b9ba7bed1bdf4
archMk <victormurithi2020@gmail.com>
2023-09-01 06:56:04 +0300

    Its a palidrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,66 @@
+#include "main.h"
+/**
+ * _strlen_rc - length of a string
+ * @s: pointer to a string
+ * Return: length of the string
+ */
+int _strlen_rc(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_rc(s + 1));
+}
+/**
+ * is_palindrome_rc - matching strings even when flipped or mirrored
+ * @i: point to the first element int the array
+ * @j: holds the length of the string
+ * @s: pointer to a string of char
+ * Return: 1 match found else return 0
+ */
+int is_palindrome_rc(char *s, int i, int j)
+{
+	/* checks for matching chars */
+	if (i >= j)
+		return (1);
+	/* non-matching chars found also our exit point*/
+	if (s[i] != s[j])
+		return (0);
+	return (is_palindrome_rc(s, i + 1, j - 1));
+}
+/**
+ * is_palindrome - matching strings even when flipped or mirrored
+ * @s: pointer to a string of char
+ * Return: 1 match found else return 0
+ */
+int is_palindrome(char *s)
+{
+	int j = _strlen_rc(s);
+
+	if (!s)
+		return (0);
+	if (j <= 1)
+		return (1);
+	return (is_palindrome_rc(s, 0, (j - 1)));
+}
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = is_palindrome("level");
+    printf("%d\n", r);
+    r = is_palindrome("redder");
+    printf("%d\n", r);
+    r = is_palindrome("test");
+    printf("%d\n", r);
+    r = is_palindrome("step on no pets");
+    printf("%d\n", r);
+    return (0);
+}
