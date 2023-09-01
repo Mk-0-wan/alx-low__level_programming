ab8d170a2bb9e36282a26dc271d409673a7ac2e5
archMk <victormurithi2020@gmail.com>
2023-09-01 06:57:17 +0300

    Its a palidrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -42,25 +42,3 @@ int is_palindrome(char *s)
 		return (1);
 	return (is_palindrome_rc(s, 0, (j - 1)));
 }
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int r;
-
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("redder");
-    printf("%d\n", r);
-    r = is_palindrome("test");
-    printf("%d\n", r);
-    r = is_palindrome("step on no pets");
-    printf("%d\n", r);
-    return (0);
-}
