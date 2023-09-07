7237aeb81d41d10c464f1a83b611fa0c4fc97b72
archMk <victormurithi2020@gmail.com>
2023-09-07 23:59:05 +0300

    calloc_replica

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	temp = malloc(size * nmemb);
+	temp = malloc(sizeof(size) * nmemb);
 	if (!temp)
 		return (NULL);
 	/* initialize all the space to value 0 */
