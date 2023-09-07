d62105d19eeb73d22d39fcd53fc7a4f824173a04
archMk <victormurithi2020@gmail.com>
2023-09-07 19:18:53 +0300

    updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,7 +9,6 @@
 			exit(98);\
 		}\
 	} while (0)
-
 /* Header files */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +16,11 @@
 
 /* Function prototypes */
 int _putchar(char c);
+int _strlen(char *s);
 void *malloc_checked(unsigned int b);
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
