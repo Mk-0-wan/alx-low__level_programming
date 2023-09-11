c00807ab8707b7c2cd1f5c8cd00182c6da160a3f
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-09-11 14:19:05 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * _array_int - array of integers
  * @string: pointer to a string of char
@@ -8,29 +7,29 @@
  */
 long int *_array_int(char *string)
 {
-	long int count = 0, i, temp, arr;
-	long int *array;
+    long int count = 0, i, temp, arr;
+    long int *array;
 
 
-	arr = _atoi(string);
-	temp = arr;
-	while (temp > 0)
-	{
-		temp /= 10;
-		count++;
-	}
-	array = malloc(count * sizeof(long int));
-	MALLOC_CHECK(array);
+    arr = atoi(string);
+    temp = arr;
+    while (temp > 0)
+    {
+        temp /= 10;
+        count++;
+    }
+    array = malloc(count * sizeof(long int));
+    MALLOC_CHECK(array);
 
-	i = count - 1;
-	while (i > 0)
-	{
-		array[i] = arr % 10;
-		arr /= 10;
-		i--;
-	}
+    i = count - 1;
+    while (i >= 0)
+    {
+        array[i] = arr % 10;
+        arr /= 10;
+        i--;
+    }
 
-	return (array);
+    return (array);
 }
 /**
  * _intlen - gets the length of int
@@ -39,17 +38,17 @@ long int *_array_int(char *string)
  */
 long int _intlen(char *s1)
 {
-	long int temp, inTs, count;
+    long int temp, inTs, count;
 
-	inTs = _atoi(s1);
-	temp = inTs;
-	count = 0;
-	while (temp > 0)
-	{
-		temp /= 10;
-		count++;
-	}
-	return (count - 1); /* avoid going overboard */
+    inTs = atoi(s1);
+    temp = inTs;
+    count = 0;
+    while (temp > 0)
+    {
+        temp /= 10;
+        count++;
+    }
+    return (count - 1); /* avoid going overboard */
 }
 /**
  * _c_mul - advanced multiplication logic method.
@@ -59,46 +58,47 @@ long int _intlen(char *s1)
  */
 void _c_mul(char *array1, char *array2)
 {
-	long int num1, num2, i = 0, j = 0, *a, *b;
-	long int  product, *results, position;
-	long int digit1, digit2;
-	long int len = 0;
+    long int num1, num2, i = 0, j = 0, *a, *b;
+    long int  product, *results, position;
+    long int digit1, digit2;
+    long int len = 0;
 
-	a = _array_int(array1);
-	b = _array_int(array2);
+    a = _array_int(array1);
+    b = _array_int(array2);
 
-	digit1 = _intlen(array1);
-	digit2 = _intlen(array2);
+    digit1 = _intlen(array1);
+    digit2 = _intlen(array2);
+    
+    results = calloc((digit1 + digit2 + 2), sizeof(long int));
+    MALLOC_CHECK(results);
 
-	for (; i < digit1; i++)
-	{
-		for (; j < digit2; j++)
-		{
-			num1 = a[(digit1 + 1) - 1 - i]; /* positions */
-			num2 = b[(digit2 + 1) - 1 - j];
-			position = (((digit1 + 1) + (digit2 + 1)) - 1 - i - j);
-			product = num1 * num2;
+    for (; i <= digit1; i++)
+    {
+        for (; j <= digit2; j++)
+        {
+            num1 = a[(digit1 + 1) - 1 - i]; /* positions */
+            num2 = b[(digit2 + 1) - 1 - j];
+            position = (((digit1 + 1) + (digit2 + 1)) - 1 - i - j);
+            product = num1 * num2;
 
-			results = _calloc((digit1 + digit2 + 2), sizeof(long int));
-			MALLOC_CHECK(results);
+            results[position] += product;
 
-			results[position] += product;
-
-			if (results[position] > 9)
-			{
-				results[position - 1] += product / 10;
-				results[position] += product % 10;
-			}
-		}
-	}
-	len = ((digit1 + digit2) + 2);
-	for (i = 0; i < len - 1; i++)
-	{
-		printf("%ld", results[i]);
-	}
-	free(a);
-	free(b);
-	free(results);
+            if (results[position] > 9)
+            {
+                results[position - 1] += results[position] / 10;
+                results[position] %= 10;
+            }
+        }
+        j = 0;
+    }
+    len = ((digit1 + digit2) + 2);
+    for (i = 0; i < len; i++)
+    {
+        printf("%ld", results[i]);
+    }
+    free(a);
+    free(b);
+    free(results);
 }
 
 int main(int argc, char *argv[])
