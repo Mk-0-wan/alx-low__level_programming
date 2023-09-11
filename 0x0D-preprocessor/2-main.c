7dfe57eb61e66e2356852caff3692c2d998ef516
archMk <victormurithi2020@gmail.com>
2023-09-11 16:49:07 +0300

    Print me

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - simple c program that prints out the name of file
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE_NAME__);
+	return (0);
+}
