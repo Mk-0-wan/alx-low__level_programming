798b8135523aa4957b4aca3bd2e0b7529279e2df
archMk <victormurithi2020@gmail.com>
2023-09-13 13:58:30 +0300

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - simple function pointer which prints a string of chars.
+ * @name: pointer to string of chars aka {name given}
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name)
+		return;
+	return (f(name));
+}
