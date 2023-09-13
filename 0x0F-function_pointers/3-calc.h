4158ea7664447fb3422609ae51346ea6cac4d7f9
archMk <victormurithi2020@gmail.com>
2023-09-13 15:02:15 +0300

    3-get_op_func.c

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,27 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+/* function prototypes */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int _putchar(char s);
+int _strcmp(char *s1, char *s2);
+int (*get_op_func(char *s))(int, int);
+int _atoi(char *s);
+
+#endif
