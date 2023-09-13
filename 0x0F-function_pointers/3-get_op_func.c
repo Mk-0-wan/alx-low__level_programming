052e8d308c1e42aaa9d712001e40e4c9959e636c
archMk <victormurithi2020@gmail.com>
2023-09-13 23:31:16 +0300

    return the option

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,32 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <string.h>
 
-/**
- * _strcmp - compare strings
- * @s1: pointer to string 1
- * @s2: pointer to string 2
- * Return: 0 success 1 not same -1 fails
- */
-int _strcmp(char *s1, char *s2)
-{
-	unsigned int i = 0;
-
-	while (*s1 != '\0' && *s2 != '\0')
-	{
-		if (s1[i] < s2[i])
-			return (-1);
-		else if (*s1 > *s2)
-			return (1);
-		s1++;
-		s2++;
-	}
-	if (*s1 == '\0' && *s2 == '\0')
-		return (0);
-	else if (*s1 == '\0')
-		return (-1);
-	else
-		return (1);
-}
 /**
  * get_op_func - get the operation fucntion
  * @s: pointer to a string of char
@@ -46,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (!_strcmp(s, ops[i].op))
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
