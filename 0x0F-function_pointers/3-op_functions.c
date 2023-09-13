c64f10939543d3421e635def1272fd2bbc95de15
archMk <victormurithi2020@gmail.com>
2023-09-13 15:03:33 +0300

    3-get_op_func.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function that performs addition
+ * @a: int parameter to be passed
+ * @b: int parameter to be passed
+ * Return: sum of the two int values
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that performs subtracts
+ * @a: int parameter to be passed
+ * @b: int parameter to be passed
+ * Return: difference of the two int values
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that performs multiplication
+ * @a: int parameter to be passed
+ * @b: int parameter to be passed
+ * Return: multiplication of the two int values
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function that performs division
+ * @a: int parameter to be passed
+ * @b: int parameter to be passed
+ * Return: divided of the two int values
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function that performs modulus
+ * @a: int parameter to be passed
+ * @b: int parameter to be passed
+ * Return: remainder of the two int values
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
