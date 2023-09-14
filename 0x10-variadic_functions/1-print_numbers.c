b93b21fe3ee3a0ecb56e323bbb4656c937772005
archMk <victormurithi2020@gmail.com>
2023-09-14 14:31:56 +0300

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i = 0;
 
-	if (!separator)
-		return;
 	va_start(ap, n);
 	i = 0;
 	for (; i < n; i++)
@@ -20,9 +18,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		printf("%d", va_arg(ap, unsigned int));
 
-		if (i != n - 1)
+		if (separator && i != n - 1)
 			printf("%s", separator);
 	}
-	va_end(ap);
 	_putchar('\n');
+	va_end(ap);
 }
