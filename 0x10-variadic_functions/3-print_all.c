5f1cff4b42baa7ae211bb749b9cabc021c34964b
archMk <victormurithi2020@gmail.com>
2023-09-15 03:31:21 +0300

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,96 @@
+#include "variadic_functions.h"
+
+/**
+ * _char_arg - prints only char arguments
+ * @ap: list of indefinite number of arguments
+ * Return: void only prints out expected result
+ */
+void _char_arg(va_list *ap)
+{
+	char c;
+
+	c = va_arg(*ap, int);
+	printf("%c", c);
+}
+/**
+ * _int_arg - prints only integer arguments
+ * @ap: list of indefinite number of arguments
+ * Return: void only prints out expected result
+ */
+
+void _int_arg(va_list *ap)
+{
+	int i;
+
+	i = va_arg(*ap, int);
+	printf("%d", i);
+}
+/**
+ * _float_arg - prints only float arguments
+ * @ap: list of indefinite number of arguments
+ * Return: void only prints out expected result
+ */
+
+void _float_arg(va_list *ap)
+{
+	float f;
+
+	f = va_arg(*ap, double);
+	printf("%f", f);
+}
+/**
+ * _string_arg - prints only string arguments
+ * @ap: list of indefinite number of arguments
+ * Return: void only prints out expected result
+ */
+
+void _string_arg(va_list *ap)
+{
+	char *s;
+
+	s = va_arg(*ap, char *);
+
+	/* check for empty strings */
+	if (!s || *s == '\0')
+		printf("(nil)");
+
+	printf("%s", s);
+}
+/**
+ * print_all - prints all datatype pass
+ * @format: string pointer
+ * Return: void
+ */
+void print_all(const char *const format, ...)
+{
+	va_list ap;
+	int form_type_no, i = 0;
+	char *separator = "";
+	format_x data[] = {
+		{'c', _char_arg},
+		{'i', _int_arg},
+		{'f', _float_arg},
+		{'s', _string_arg},
+		{0, NULL}
+	};
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		/* return the first position you find a match */
+		form_type_no = 0;
+		while (data[form_type_no].call && format[i])
+			form_type_no++;
+
+		/* found form_type_no do this then */
+		if (data[form_type_no].call)
+		{
+			printf("%s", separator);
+			data[form_type_no].call(&ap);
+			separator = ", ";
+		}
+		i++; /* move to the next char in line */
+	}
+	va_end(ap);
+	printf("\n");
+}
