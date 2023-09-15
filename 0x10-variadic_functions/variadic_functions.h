dbc9a9c1a553f97f469e94f276fc02733237abd3
archMk <victormurithi2020@gmail.com>
2023-09-15 03:31:58 +0300

    last header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,8 +4,17 @@
 /* Included libraries */
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+/* Structure */
+typedef struct format_specifier
+{
+	char form;
+	void (*call)(va_list *);
+}format_x;
 
 /* Function prototypes */
+int _putchar(char s);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
