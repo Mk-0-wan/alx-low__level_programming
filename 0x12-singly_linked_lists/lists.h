fe79300ce6fb06555d49b5f5625b0f3ccdaa9c8e
archMk <victormurithi2020@gmail.com>
2023-09-21 16:57:46 +0300

    header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,31 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+/* Library Files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* Function prototypes */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *s);
+
+#endif
