332133402e3c7e8164c464798a189f00c21a5434
archMk <victormurithi2020@gmail.com>
2023-09-21 22:58:04 +0300

    fixed checker 8 & 7

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,6 @@ void free_list(list_t *head)
 {
 	list_t *temp, *current = head;
 
-	if (!current)
-	{
-		_puts("Nothing to free here\n");
-		return;
-	}
-
 	while (current)
 	{
 		temp = current->next;
