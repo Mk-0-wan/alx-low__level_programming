e2fd903ac2f896c67c83fa5f311705ac8514132b
archMk <victormurithi2020@gmail.com>
2023-09-21 16:56:44 +0300

    quiz one

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,70 @@
+#include "lists.h"
+
+/**
+ * _strlen - get the length of a string
+ * @s: pointer to a string of chars
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _puts - output the strings to the stdio
+ * @s: pointer to a string of chars
+ * Return: void
+ */
+void _puts(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+}
+
+/**
+ * print_list - prints all the values int the linked list
+ * @h: pointer to the first node in the linked list
+ * Return: number of nodes int the linked list.
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int len = 0;
+	unsigned int count = 0;
+	const list_t *current = h;
+
+	if (!h)
+		return (-1);
+
+	while (current != NULL)
+	{
+		if (!current->str)
+		{
+			_putchar('[');
+			_putchar('0');
+			_putchar(']');
+			_putchar(' ');
+			_puts("(nil)");
+		}
+		else
+		{
+			len = _strlen(current->str);
+			_putchar('[');
+			_putchar(len + '0');
+			_putchar(']');
+			_putchar(' ');
+			_puts(current->str);
+		}
+		_putchar('\n');
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
