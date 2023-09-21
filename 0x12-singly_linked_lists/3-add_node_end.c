d5f4c6c6cbee48205ac63194c17e1fdb9524441c
archMk <victormurithi2020@gmail.com>
2023-09-21 18:59:53 +0300

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,12 +9,11 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *current;
+	list_t *new_node, *current = *head;
 	unsigned int len = 0;
 
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
@@ -22,18 +21,20 @@ list_t *add_node_end(list_t **head, const char *str)
 		_puts("Memory allocation failed\n");
 		return (NULL);
 	}
+
+	new_node->len = len;
+	new_node->str = strdup(str);
+	new_node->next = NULL;
+
 	if (!*head)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-	while (str[len])
-		len++;
-
+	while (current->next)
+		current = current->next;
 	current->next = new_node;
-	new_node->len = len;
-	new_node->str = strdup(str);
-	new_node->next = NULL;
+
 	return (new_node);
 }
