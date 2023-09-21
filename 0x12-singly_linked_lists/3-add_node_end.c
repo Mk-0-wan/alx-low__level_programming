d8920da6c230178ad588ed4cbc5b67a1c476c9ab
archMk <victormurithi2020@gmail.com>
2023-09-21 18:42:43 +0300

    add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - adds a node at the end of the list
+ * @head: address of the first node in the list
+ * @str: string of chars
+ * Return: address of the newly created node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *current;
+	unsigned int len = 0;
+
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+	{
+		_puts("Memory allocation failed\n");
+		return (NULL);
+	}
+
+	while (str[len])
+		len++;
+
+	current->next = new_node;
+	new_node->len = len;
+	new_node->str = strdup(str);
+	new_node->next = NULL;
+	return (new_node);
+}
