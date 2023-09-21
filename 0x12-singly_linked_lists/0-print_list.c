e796fb122c19bfd770f337b16221d988e86f9e0d
archMk <victormurithi2020@gmail.com>
2023-09-21 23:06:13 +0300

    solved for few checks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 	unsigned int len;
 	const char *s;
-	list_t *current = (list_t *)h;
+	const list_t *current = h;
 
 	while (current != NULL)
 	{
