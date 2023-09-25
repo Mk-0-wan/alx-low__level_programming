c5440d0275293d6b58602e26f8af49512f702ec8
archMk <victormurithi2020@gmail.com>
2023-09-25 10:27:14 +0300

    pop me first

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - removes the head node
+ * @head: pointer to the first node
+ * Return: head node data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *new_node;
+	int ret = 0;
+
+	if (!*head)
+	{
+		printf("Appears you have an empty list\n");
+		return (0);
+	}
+	new_node = (*head)->next; /* store the next node */
+	ret = (*head)->n; /* collect head node val */
+	free(*head);
+	*head = new_node; /* make new node the new head node */
+	return (ret);
+}
