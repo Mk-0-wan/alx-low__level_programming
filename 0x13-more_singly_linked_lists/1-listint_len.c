4fa2816ec4fea4b6552032c97a4cec656aaf1f29
archMk <victormurithi2020@gmail.com>
2023-09-26 02:23:12 +0300

    print len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -16,12 +16,6 @@ size_t listint_len(const listint_t *h)
 {
 	unsigned int node_len = 0;
 
-	if (!h)
-	{
-		printf("Appears you have an empty list\n");
-		return (-1);
-	}
-
 	while (h)
 	{
 		h = h->next;
