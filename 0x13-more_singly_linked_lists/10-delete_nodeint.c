39210992c8ae414b5c1d5b0d197eb0e9a065588d
archMk <victormurithi2020@gmail.com>
2023-09-26 02:59:44 +0300

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 
-	if (!*head)
+	if (!head || !*head)
 		return (-1);
 	current = *head;
 	if (index == 0)
