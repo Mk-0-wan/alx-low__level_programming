63d916bf5cc1d79cfaa33195305a9a20f966eb3e
archMk <victormurithi2020@gmail.com>
2023-09-27 00:17:24 +0300

    free-node-safely

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * free_listint_safe - frees all the node is the safest way possible
+ * by only looping over the loop once
+ * @h: pointer to the first node in the list
+ * Return: length of the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current;
+	size_t n_nodes = 0;
+
+	if (!h)
+		return (n_nodes);
+
+	while (h)
+	{
+		n_nodes++;
+		current = (*h)->next;
+		free((*h));
+		if (current && (void *)current < (void *)(*h))
+		{
+			*h = current;
+		}
+		else
+			break;
+	}
+	*h = NULL;
+	return (n_nodes);
+}
