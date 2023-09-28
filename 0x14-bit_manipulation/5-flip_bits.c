d56fba681e95d4e6c6ace4532f0b87a3dd697a58
archMk <victormurithi2020@gmail.com>
2023-09-29 02:29:50 +0300

    flipping bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,16 +7,17 @@
  * @m: second integer to compare to n
  * Return: unsigned int of number of bits to flip
  */
-unsigned int flip_bits(unsigned long n, unsigned long m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int bits_to_flip = 0;
 	/* exclusive or of the two int's */
-	unsigned long int xor_val;
+	unsigned long int x0r_val = (n ^ m);
 
-	xor_val = n ^ m;
-	for (; xor_val > 0 ; xor_val >>= 1)/* making sure all the bits value are used up */
+	while (x0r_val > 0) /* making sure all the bits value are used up */
 	{
-		bits_to_flip += xor_val & 1; /* check the LSB */
+		bits_to_flip += x0r_val & 1; /* check the LSB */
+		x0r_val >>= 1;/* divide by 2 to always get the LSB */
 	}
 	return (bits_to_flip);
 }
+
