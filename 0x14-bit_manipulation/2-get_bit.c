1087d78590b82aaa37c3aa65d63f5278f5413a78
archMk <victormurithi2020@gmail.com>
2023-09-29 01:53:43 +0300

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,7 @@
  *
  * Return: bit value at that position
  */
-int get_bit(unsigned long n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 0x01;
 	int r;
