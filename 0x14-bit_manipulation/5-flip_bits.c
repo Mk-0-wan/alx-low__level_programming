f016c1d0375c93fedd8e995c7b4027e62f7ae8d5
archMk <victormurithi2020@gmail.com>
2023-09-29 00:42:02 +0300

    bits to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -20,17 +20,3 @@ unsigned int flip_bits(unsigned long n, unsigned long m)
 	}
 	return (bits_to_flip);
 }
-int main(void)
-{
-    unsigned int n;
-
-    n = flip_bits(1024, 1);
-    printf("%u\n", n);
-    n = flip_bits(402, 98);
-    printf("%u\n", n);
-    n = flip_bits(1024, 3);
-    printf("%u\n", n);
-    n = flip_bits(1024, 1025);
-    printf("%u\n", n);
-    return (0);
-}
