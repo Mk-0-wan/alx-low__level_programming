5edcec47f000d5ced8b1674f6b003e668ba1ea24
archMk <victormurithi2020@gmail.com>
2023-09-29 01:33:57 +0300

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,10 +11,10 @@ int clear_bit(unsigned long *n, unsigned int index)
 	unsigned long int mask = 0x01;
 
 	mask <<= index; /* set a bit field according to index */
-	if (mask == 0 || index >= (sizeof(unsigned long int) * CHAR_BIT))
+	if (index >= (sizeof(unsigned long int) * CHAR_BIT))
 		return (-1);
 
-	*n &= mask;/* enable the bit */
+	*n &= ~mask;/* enable the bit */
 
 	return (1);
 
