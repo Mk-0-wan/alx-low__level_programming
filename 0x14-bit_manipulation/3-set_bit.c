d9179b8d483c37d79f3a12e66d5bfed019fd9252
archMk <victormurithi2020@gmail.com>
2023-09-29 01:20:45 +0300

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - setting bits at a given position
+ * @n: integer to get the check the bit from
+ * @index: position of the bit
+ */
+int set_bit(unsigned long *n, unsigned int index)
+{
+	unsigned long int mask = 0x01;
+
+	mask <<= index; /* set a bit field according to index */
+	if (mask == 0 || index >= (sizeof(unsigned long int) * CHAR_BIT))
+		return (-1);
+
+	*n |= mask;/* enable the bit */
+
+	return (1);
+}
