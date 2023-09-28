4dd1eed06d3fcb6ab71163d1ae352ec665c3abb4
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-09-29 02:22:44 +0300

    Get use to XOR

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,14 +9,14 @@
  */
 unsigned int flip_bits(unsigned long n, unsigned long m)
 {
-	unsigned long bits_to_flip = 0;
+	unsigned int bits_to_flip = 0;
 	/* exclusive or of the two int's */
-	unsigned long x0r_val = (n ^ m);
+	unsigned long int xor_val = (n ^ m);
 
-	while (x0r_val > 0) /* making sure all the bits value are used up */
+	while (xor_val > 0) /* making sure all the bits value are used up */
 	{
-		bits_to_flip += x0r_val & 1; /* check the LSB */
-		x0r_val >>= 1;/* divide by 2 to always get the LSB */
+		bits_to_flip += xor_val & 1; /* check the LSB */
+		xor_val >>= 1;/* divide by 2 to always get the LSB */
 	}
 	return (bits_to_flip);
 }
