417fd08e6e6714a93194bc3a1decce18e5fa34b0
archMk <victormurithi2020@gmail.com>
2023-09-29 10:02:23 +0300

    handled unsigned int issues

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,50 +2,6 @@
 #include <limits.h>
 
 /**
- * _pow_recursion - replica of the pow function in c
- * @x: value to calculate the power from
- * @y: value holding the power
- * Return: an int holding the power value
- */
-uli _pow_recursion(uli x, uli y)
-{
-	/*if (y < 0)  both exit points at -1 and 1*/
-		/*return (-1);*/
-
-	if (y == 0)
-		return (1);
-	/* 2 x 2 ^ (y - 1) */
-	return (x * _pow_recursion(x, (y - 1)));
-	/* value multiplied by the power of the remaining powered values minus 1 */
-}
-
-/**
- * _atoi - return an int value from a string
- * @str: string value passed
- * Return: the int value from a string
- */
-uli _atoi(char *str)
-{
-	int sign = 1, i = 0;
-	uli result = 0;
-
-	while (!(str[i] <= '9' && str[i] >= '0') && str[i] != '\0')
-	{
-		if (str[i] == '-')
-		{
-			sign *= -1;
-		}
-		i++;
-	}
-	while (str[i] <= '9' && (str[i] >= '0' && str[i] != '\0'))
-	{
-		result = (result * 10) + (str[i] - '0');
-		i++;
-	}
-	result *= sign;
-	return (result);
-}
-/**
  * binary_to_uint - transforms string binary to int.
  * @b: pointer to a memory location containing bit representation of number
  * Return: digit value of the binary representation
@@ -53,9 +9,6 @@ uli _atoi(char *str)
 unsigned int binary_to_uint(const char *b)
 {
 	uli dec = 0;
-	uli pos = 0;
-	uli rem = 0;
-	uli bin = 0;
 	uli i = 0;
 	char *local;
 
@@ -70,17 +23,8 @@ unsigned int binary_to_uint(const char *b)
 		if (local[i] != '1' && local[i] != '0')
 			return (0);
 
-		else
-			bin = _atoi(local);
+		dec = dec * 2 + (local[i] - '0');
 		i++;
-	}
-
-	while (bin > 0)
-	{
-		rem = bin % 10;
-		bin /= 10;
-		dec += rem  * _pow_recursion(2, pos);
-		pos++;
 	}
 	return (dec);
 }
