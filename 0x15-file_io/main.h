4157bf13997852795fbfab1e218e48dd9dc24cc2
archMk <victormurithi2020@gmail.com>
2023-10-02 23:13:45 +0300

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,14 @@
-#ifndef _FILE_IO_
-#define _FILE_IO_
-/* Header files */
+#ifndef _ALX_FILE_IO_
+#define _ALX_FILE_IO_
+
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-/* Custom function pointers */
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 #endif
