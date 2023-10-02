ce5b8e75da6d140f7ad7b15f4a55c165bcad563b
archMk <victormurithi2020@gmail.com>
2023-10-02 23:01:50 +0300

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,35 +10,39 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, i = 0;
 	int size_of_read_write = 0, ret = 0;
-	char *local_buffer = malloc(sizeof(char) * letters);
+	char *local_buffer = NULL;
 
-	if (!filename || !local_buffer || !letters) /* check for empty buffers */
+	if (!filename || !letters) /* check for empty buffers */
 		return (0);
 
 	fd = open(filename, O_RDONLY);/* read only mode */
 	if (fd == -1)
 	{
-		free(local_buffer);
 		return (0);
 	}
+	local_buffer = malloc(sizeof(char) * letters);
+	if (!local_buffer)
+		return (0);
 	size_of_read_write = read(fd, local_buffer, letters); /* reading file */
 	if (size_of_read_write == -1)
 	{/* handling read */
-		free(local_buffer);
-		close(fd);
 		return (0);
 	}
 	close(fd);
-	while (local_buffer[i])
-		i++;
-	size_of_read_write = write(STDOUT_FILENO, local_buffer, i);
-	/* write to STDOUT */
-	if (size_of_read_write == -1)
-	{
+	if (size_of_read_write < 0)
+	{/* handling read */
 		free(local_buffer);
 		return (0);
 	}
-	ret = size_of_read_write;/* keep record of the last write system call */
+	if (!size_of_read_write)
+		i = letters;
+	size_of_read_write = write(STDOUT_FILENO, local_buffer, i);
 	free(local_buffer); /* avoid memory leaks */
+	/* write to STDOUT */
+	if (size_of_read_write < 0)
+	{
+		return (0);
+	}
+	ret = size_of_read_write;/* keep record of the last write system call */
 	return ((ssize_t)ret);
 }
