dd3837b7e8cad168b7b49bac242ce178a9bbc6cd
archMk <victormurithi2020@gmail.com>
2023-11-14 03:42:06 +0300

    shoot this task

--- 0x18-dynamic_libraries/generate_rand.c
+++ 0x18-dynamic_libraries/generate_rand.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int iterations_of_rand = 0;
 /**
  * rand - generate random numbers
  * Return: int values
  */
 int rand(void)
 {
+	static int iterations_of_rand;
 	int prdef_values[] = {9, 8, 10, 24, 75, 9};
 
+	iterations_of_rand = 0;
 	return ((iterations_of_rand < 6) ? prdef_values[iterations_of_rand++] : 22);
 }
