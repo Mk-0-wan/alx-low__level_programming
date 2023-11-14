cc083838ff745b958860ccaf6dd30fd5b8a57c34
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-11-14 14:17:50 +0300

    Update generate_rand.c

--- 0x18-dynamic_libraries/generate_rand.c
+++ 0x18-dynamic_libraries/generate_rand.c
@@ -10,5 +10,5 @@ int rand(void)
 	static int prdef_values[] = {9, 8, 10, 24, 75, 9};
 
 	iterations_of_rand = 0;
-	return ((iterations_of_rand < 6) ? prdef_values[iterations_of_rand++] : 22);
+	return ((iterations_of_rand < 6) ? prdef_values[iterations_of_rand++] : 0);
 }
