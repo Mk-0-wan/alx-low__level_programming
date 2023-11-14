29fcc34b0d1b7a4c2478665677531170daaaa782
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-11-14 14:03:40 +0300

    Ginja millions fix

--- 0x18-dynamic_libraries/generate_rand.c
+++ 0x18-dynamic_libraries/generate_rand.c
@@ -7,7 +7,7 @@
 int rand(void)
 {
 	static int iterations_of_rand;
-	int prdef_values[] = {9, 8, 10, 24, 75, 9};
+	static int prdef_values[] = {9, 8, 10, 24, 75, 9};
 
 	iterations_of_rand = 0;
 	return ((iterations_of_rand < 6) ? prdef_values[iterations_of_rand++] : 22);
