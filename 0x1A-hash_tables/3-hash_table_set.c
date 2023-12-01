a746890dee54548a32fe00bfac4248fb6d4e771b
archMk <victormurithi2020@gmail.com>
2023-12-02 02:19:43 +0300

    fixed some error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *bucket = NULL
+	hash_node_t *bucket = NULL;
 	char *tmp;
 	hash_node_t *current = NULL;
 	ul indx;
