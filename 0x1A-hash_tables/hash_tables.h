4b6608f54a9bc135404d0f8cae92d1fc03b02d08
archMk <victormurithi2020@gmail.com>
2023-12-01 03:49:03 +0300

    adding new func

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,6 +43,7 @@ hash_table_t *hash_table_create(ul size);
 ul hash_djb2(const uc *str);
 ul key_index(const uc *key, ul size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_node_t *buckets(const char *key, const char *value);
 void free_all_buckets(hash_node_t *buckets);
 void free_hash_table(hash_table_t *ht);
 
