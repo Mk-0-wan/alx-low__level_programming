0f1d5c5aa1bf5b9c3e547bca32695a057d0597d6
archMk <victormurithi2020@gmail.com>
2023-12-02 03:01:10 +0300

    printing values

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -20,16 +20,13 @@ void hash_table_print(const hash_table_t *ht)
 		buckets = ht->array[iter];
 		while (buckets)
 		{
+			if (flag)
+				printf(", ");
 			printf("'%s': '%s'", buckets->key, buckets->value);
 			/* separate nodes in the buckets key value pairs */
-			if (buckets->next)
-				printf(", ");
 			buckets = buckets->next;
-			flag++;
+			flag = 1;
 		}
-		/* separate buckets key value pairs */
-		if (ht->array[iter + 1] && flag > 0)
-			printf(", ");
 	}
 	printf("}\n");
 }
