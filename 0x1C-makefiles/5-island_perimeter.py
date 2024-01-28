f9d1094d20006ef43ca7f130f94d1b736e44ca3d
tmk_ubuntu <ubuntu0mk@gmail.com>
2024-01-29 00:49:25 +0300

    Adding commentsc

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python3
 # the Island Perimeter solution
+
 def island_perimeter(grid=list) -> int:
-    # will need to get the dimensions of the grid
+    """Island Perimeter Calculator
+    Args:
+        grid (list): a list of lists where island is
+        represented by value of 1
+    Return:
+        returns the total perimeter of the islands found
+
+    try running the file as a main file to see the result
+    Steps:
+       1. will need to get the dimensions of the grid
+       2. iter for number of islands
+       3. iter for number of neighbours
+       4. looping over each element in the grid
+       5. check for all the leftside neighbours
+       6. check for all the rightside neighbours
+       7. check for all the rightside neighbours
+       8. check for all the leftside neighbours
+       9. get the theoretical proof
+       10.the actual value
+    """
     height = len(grid)
     width = len(grid[0])
-    # iter for number of islands
     count = 0
-    # iter for number of neighbours
     touch = 0
-    # looping over each element in the grid
     for x in range(height):
         for y in range(width):
             if grid[y][x] == 1:
                 count += 1
-                # check for all the leftside neighbours
                 if y - 1 >= 0 and grid[x][y - 1] == 1:
                         touch += 1
-                # check for all the rightside neighbours
                 if y + 1 < width and grid[x][y + 1] == 1:
                         touch += 1
-                # check for all the rightside neighbours
                 if x + 1 < height and grid[x + 1][y] == 1:
                         touch += 1
-                # check for all the leftside neighbours
                 if x - 1 >= 0 and grid[x - 1][y] == 1:
                         touch += 1
-    # get the theoretical proof
     total_perimeter = count * 4
-    # the actual value
     return (total_perimeter - touch)
 
 if __name__ == "__main__":
