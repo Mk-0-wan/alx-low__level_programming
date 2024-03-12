98b7f3a7bcd4e37d348e3d598f1b09a2680667b9
mk <victormurithi2020@gmail.com>
2024-03-12 16:56:05 +0300

    Fix betty error

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,8 +5,12 @@
 #include <math.h>
 #include <stdlib.h>
 #define Length(A) ((sizeof(A)) / (sizeof(A[0])))
-#define Min(z, h) ((z) + (((h) - (z)) & (((h) - (z)) >> (((sizeof(int)) * (8) - (1))))))
-#define Max(z, h) ((h) - (((h) - (z)) & (((h) - (z)) >> (((sizeof(int)) * (8) - (1))))))
+#define Min(z, h)\
+	((z) + (((h) - (z)) &\
+		(((h) - (z)) >> (((sizeof(int)) * (8) - (1))))))
+#define Max(z, h)\
+	((h) - (((h) - (z)) &\
+		(((h) - (z)) >> (((sizeof(int)) * (8) - (1))))))
 #define MASK(x) ((x) >> ((sizeof(int)) * (8) - (1)))
 #define Absolute(y) (((y) + (MASK(y))) ^ (MASK(y)))
 
