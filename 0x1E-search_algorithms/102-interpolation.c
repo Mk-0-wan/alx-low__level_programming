ccf15be185400217d68a825dc9354c1421da83f3
mk <victormurithi2020@gmail.com>
2024-03-14 06:25:45 +0300

    value out of range

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -24,6 +24,13 @@ int interpolation_search(int *array, size_t size, int value)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 				* (value - array[low]));
+		if (pos > size)
+		{
+			printf("Value checked array[%d] is out of range\n",
+			       (int)pos);
+			return (-1);
+		}
+
 		printf("Value checked array[%d] = [%d]\n", (int)pos, array[pos]);
 		if (low == high)
 		{
