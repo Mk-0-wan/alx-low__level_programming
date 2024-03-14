414f568d573e1bbafe4bf3b5570c82d9c6ce89c1
mk <victormurithi2020@gmail.com>
2024-03-14 05:56:50 +0300

    Updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -20,6 +20,7 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 /* Custom helper functions */
 int btr(int *array, int T, int start, int end);
 #endif
