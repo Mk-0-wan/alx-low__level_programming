7f5ab72c99927b57f4e15e79261ea19114033d4e
mk <victormurithi2020@gmail.com>
2024-03-14 05:16:03 +0300

    Exponential search prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -19,5 +19,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-
+int exponential_search(int *array, size_t size, int value);
+/* Custom helper functions */
+int btr(int *array, int T, int start, int end);
 #endif
