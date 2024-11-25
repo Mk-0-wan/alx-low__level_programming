4b5c4f494cd486bcef56d0e3ee42d3d3edf23909
gh057 <victormurithi2020@gmail.com>
2024-11-26 01:37:18 +0300

    fix: added all the missing files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,7 @@
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the resultof the multiplication
+ */
+int mul(int a, int b) { return (a * b); }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ * @n: number of times the character _ should be printed
+ * Return: void
+ */
+void print_line(int n) {
+  int i = 0;
+
+  while (i < n && n > 0) {
+    _putchar('_');
+    i++;
+  }
+  _putchar('\n');
+}
