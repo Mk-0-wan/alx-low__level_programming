19c6cb195bc69c26f8d43ffa7c8a006b71c5390f
gh057 <victormurithi2020@gmail.com>
2024-11-26 00:06:07 +0300

    Recap: Fixing some missing repo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,6 @@
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c) { return (c >= 'A' && c <= 'Z' ? 1 : 0); }
